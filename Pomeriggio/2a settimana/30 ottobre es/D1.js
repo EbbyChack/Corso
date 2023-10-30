24961ff6d3d6b4be2e5c639b23a8b274c1c82d41
vlirkbog <joshyebby@gmail.com>
2023-10-30 14:54:30 +0100

    ok

--- Pomeriggio/2a settimana/30 ottobre es/D1.js
+++ Pomeriggio/2a settimana/30 ottobre es/D1.js
@@ -11,38 +11,65 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* Si dividono in due tipologie: primitivi e complessi. 
+I primitivi sono: numeri, stringhe, boolean, null e undefined.
+Nella categoria complessi abbiamo gli oggetti.
+
+Numeri: Vengono scritti senza apici, possono essere negativi e decimali.
+Stringhe: Parole o frasi scritte tra le apici.
+Boolean: Può essere vero o falso
+Null: Il risultato risulta null e ha un valore
+Undefined: A differenza di null non ha un valore
+
+
+
+*/
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+var name = "Ebby";
+console.log(name);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+
+var x = 12;
+var y = 20;
+z = x + y;
+console.log(z)
+
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+var x = 12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+
+var name = "Chakkungal";
+// const namefake = Pippo;
+// var namefake = Pluto;
+console.log(name);
+
+
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+var y = 4;
+z = x - y;
+console.log(z)
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +77,8 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+name1 = "john";
+name2 = "John";
+console.log(name1 != name2);
+console.log(name1 == name2.toLowerCase());
+

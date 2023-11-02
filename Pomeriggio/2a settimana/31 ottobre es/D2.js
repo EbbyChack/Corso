805bcd36087f0e5006aedfcd05f9d81943676b18
vlirkbog <joshyebby@gmail.com>
2023-11-02 08:50:54 +0100

    ok

--- Pomeriggio/2a settimana/31 ottobre es/D2.js
+++ Pomeriggio/2a settimana/31 ottobre es/D2.js
@@ -155,6 +155,8 @@ console.log(array)
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
 
-array.pop()
-array.push(100)
+// array.pop()
+// array.push(100)
+
+array[9] = 100
 console.log(array)

b45dc35eaabaafe3fb6885243475c3c75e892561
vlirkbog <joshyebby@gmail.com>
2023-11-08 16:51:44 +0100

    ok

---
+++ Pomeriggio/3a settimana/8 novembre es/assets/scripts/todolist.js
@@ -0,0 +1,9 @@
+// tre cose diverse: una funzione che fa add task, addTask
+// una funzione che gestisce quando il mio task è finito, taskComplete
+// una funzione che cancella, deleteTask
+
+
+// document.getElementById("deletebtn").addEventListener("click", function(){
+//     document.getElementById("taskdisplay").remove();
+// })
+
